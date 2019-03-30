fb579dc034bb7ecbfacb634117e42d05e45b5187
aodus <akikazu.sudou+github@gmail.com>
2019-03-30 10:11:29 +0900

    Create datadisk.bash

---
+++ datadisk.bash
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+
+OFNAME=$(volname /dev/sr0 | tr -d " \n")
+
+if [[ -z ${OFNAME} ]]; then
+        OFNAME=$1
+        if [[ -z ${OFNAME} ]]; then
+                echo "volname not found"
+                exit 1
+        fi
+fi
+
+# check file exists
+if [[ -e ./iso/${OFNAME}.iso ]]; then
+        echo "iso file already exists"
+        exit 1
+fi
+
+ddrescue -d -n -v /dev/sr0 ./iso/${OFNAME}.iso ./iso/${OFNAME}.log && sleep 5 && eject
